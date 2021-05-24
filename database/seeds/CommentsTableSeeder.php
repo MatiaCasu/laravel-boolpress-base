3e990c725d9f3edb95e8ed1b198606cae56762e8
Matia <matia.casu@gmail.com>
2021-05-24 18:36:07 +0200

    Aggiunti dati random tramite Faker php

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comment;
+use Faker\Generator as Faker;
+use App\Post;
+
+
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $publicPost= Post::where('public', true)->get();
+
+        foreach ($publicPost as $post){
+
+            for ($i=0; $i < rand(0, 5); $i++) { 
+                $newComment = new Comment();
+
+                $newComment->author = $faker->name();
+                $newComment->content = $faker->text();
+                $newComment->post_id = $post->id;    
+
+                $newComment->save();
+            }
+        }
+    }
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++){
+        $newPost = new Post();
+
+        $newPost->title = $faker->sentence();
+        $newPost->slug = $slug = Str::slug($newPost->title, '-');
+        $newPost->content = $faker-> text();
+        $newPost->public = $faker->boolean();
+        $newPost->image = $faker-> imageUrl(360, 360, null, true);
+
+        $newPost->save();
+        }
+    }
+}
